e4407f61753257990769e2e21cb0c6aed41217a9
Lauren Conley <laurenashley.mail@gmail.com>
2020-09-04 15:38:11 -0500

    App state list of items and active status

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
 import React from 'react';
+import Item from './Item';
 
 const exampleList = [
     {
         id: 1,
         label: 'Box',
-        value: 3
+        value: 3,
+        active: true
     },
     {
         id: 2,
         label: 'Tree',
-        value: 4
+        value: 4,
+        active: true
     },
     {
         id: 3,
         label: 'House',
-        value: 5
+        value: 5,
+        active: true
     },
     {
         id: 4,
         label: 'Cat',
-        value: 3
+        value: 3,
+        active: true
     }
 ];
 
-const App = () => {
-    return <h1>Hello World</h1>;
-}
+class App extends React.Component {
+    state = {
+        exampleList,
+    };
+
+    onButtonSelect = (selectedItem, selectedIndex)=> {
+        this.setState((state)=> {
+            const exampleList = state.exampleList.map((item, index) =>{
+                if(index === selectedIndex){
+                    return item.active = !selectedItem.active;
+                } else {
+                    return item;
+                }
+            });
+            return exampleList;
+        });
+    }
+
+    total = ()=> {
+        let payload = 0;
+        this.state.exampleList.map((item)=>{
+            if(item.active) {
+                return payload = payload + item.value;
+            }
+            return payload;
+        });
+        return payload; 
+    }
+    
+    render(){
+        return (
+            <div>
+                <Item items={this.state} onButtonSelect={this.onButtonSelect}/>
+                {this.total()}
+             </div>
+        );
+    }
+    
+};
 
 export default App;
--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const Item = () => {
+
+const Item = ({items, onButtonSelect}) => {
+    
+    const renderedItems = items.exampleList.map((item, index) => {
+        return (
+            <div key={item.id} className="item-container">
+                <button className="active" 
+                onClick={()=>onButtonSelect(item, index)}
+                >
+                Deselect Item
+                </button>
+                    <h4>{item.label}</h4>
+                    <span>{item.value}</span>
+            </div>
+        );
+    });
+
+
     return (
         <div>
-            
+            <div>
+            <p>hey there</p>
+                {renderedItems}
+            </div>
         </div>
     )
 }
+
+export default Item;
